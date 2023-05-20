85c6c292f6123dd77562627920a99ebbe7341736
artemiikulikov <aapqwhat@gmail.com>
2023-05-20 16:34:01 +0300

    :hatching_chick: init commit

---
+++ marketplace/marketplace/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+]
