64f04197f5f279fa2c36e790057047b43e1bd54e
artemiikulikov <aapqwhat@gmail.com>
2023-05-22 13:05:39 +0300

    :star: feat: add signup button and form

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignupForm
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -13,3 +15,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+        })
+
